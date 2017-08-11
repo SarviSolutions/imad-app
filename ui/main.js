dd18059808ec803b9f5f7d7d39147d36450fc2a6
Sarvi Solutions <sarvisolutions@gmail.com>
2017-08-12 00:03:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,24 +1,19 @@
-//counter code
-var button = document.getElementById('counter');
-var counter = 0;
-button.onclick = function ()
-{
-  //Create a request object
-  var request = new XMLHttpRequest();
-  
-  //Capture the response and store it in a variable
-  request.onreadtstatechange = function (){
-      if (request.readtState === XMLHttpRequest.DONE){
-          //Take some action
-          if (request.status === 200){
-            var counter = request.responseById('count');
-            var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
-          }
-      }
-      //Not done yet
-  };
-  //Make the request
-  request.open('GET', 'http://sarvisolutions41.imad.hasura-app.io/counter', true);
-  request.send(null);
-};
+function exec(){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() 
+    {
+          if (request.readyState === XMLHttpRequest.DONE) 
+          {
+              //Take Some Action
+              if (request.status === 200) 
+                { 
+                    var counter = request.responseText; 
+                    var span = document.getElementById('count');
+                    span.innerHTML = counter.toString();
+                } 
+          } 
+    }; 
+    request.open("GET", "http://sarvisolutions41.imad.hasura-app.io/counter", true);
+    request.send(); 
+    console.log('EXECUTED');
+}
